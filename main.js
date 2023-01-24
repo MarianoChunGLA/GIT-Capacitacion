07eab3f48127d353703eda2cfd717233a647b233
Mariano Chun <mchun@andreani.com>
2023-01-23 22:47:20 -0300

    Revert "Add funcion getAllAccounts"
    This reverts commit 6b81f57bcb169604c59b31e39760e3cfa8aad777.

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 console.log("Holaa, soy un console.log");
-
-function getAllAccounts(){
-    alert("Error fatal! Mirar la consola");
-    throw new Error("Soy un error!! Fallo total");
-}
-
 console.log("Holaa, soy un console.log posterior a la creación de la funcion getAllAccounts()");
+
